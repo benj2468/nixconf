a87fbbed2cf3b16bba2467fa9e03910159fc4439
Benjamin Cape <benjcape@gmail.com>
2025-06-14 12:17:31 -0700

    fix fzf

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, config, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -8,6 +8,16 @@
       ZSH_TMUX_AUTOSTART=true
       ZSH_TMUX_FIXTERM=false
       ZSH_TMUX_CONFIG=$HOME/.config/tmux/tmux.conf
+
+
+      # Define an init function and append to zvm_after_init_commands
+      function my_init() {
+        # This is copied from the fzf program zsh integration, but we want to make sure we do it here...
+        if [[ $options[zle] = on ]]; then
+          source <(${lib.getExe config.programs.fzf.package} --zsh)
+        fi
+      }
+      zvm_after_init_commands+=(my_init)
     '';
 
     plugins = [
