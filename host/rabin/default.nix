310a992b6a9434a72857d6b5b038020570b2d37c
benjcape <benjcape@gmail.com>
2025-09-26 19:51:48 -0700

    remove immich

--- host/rabin/default.nix
+++ host/rabin/default.nix
@@ -47,14 +47,6 @@
     };
   };
 
-  services.immich = {
-    enable = true;
-    port = 2283;
-    environment = {
-      IMMICH_TELEMETRY_INCLUDE = "all";
-    };
-  };
-
   services.ntfy-sh = {
     enable = true;
     settings = {
@@ -84,20 +76,6 @@
         locations."/" = {
           proxyPass = "http://127.0.0.1:2586";
           proxyWebsockets = true;
-        };
-      };
-
-      "immich.haganah.net" = {
-        locations."/" = {
-          proxyPass = "http://[::1]:${toString config.services.immich.port}";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          extraConfig = ''
-            client_max_body_size 50000M;
-            proxy_read_timeout   600s;
-            proxy_send_timeout   600s;
-            send_timeout         600s;
-          '';
         };
       };
     };
