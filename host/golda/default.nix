3608653c1bebe2e5e90d8cb8b94cc55bc51257bc
benjamin cape <benjcape@gmail.com>
2025-11-23 11:17:58 -0800

    golda no vlan18

--- host/golda/default.nix
+++ host/golda/default.nix
@@ -15,8 +15,6 @@
     vlans = {
       # VLAN 11 is internet
       vlan11 = { id = 11; interface = "enu1u1"; };
-      # VLAN 18 will be local mesh, provided by this router.
-      vlan18 = { id = 18; interface = "enu1u1"; };
     };
 
     interfaces.enu1u1.useDHCP = false;
