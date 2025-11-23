06fb75bc638d2b3f3f8f6bf635cade9faae00f31
benjamin cape <benjcape@gmail.com>
2025-11-23 13:31:24 -0800

    status ip for the manager

--- host/golda/default.nix
+++ host/golda/default.nix
@@ -12,7 +12,13 @@
       vlan11 = { id = 11; interface = "enu1u1"; };
     };
 
-    interfaces.enu1u1.useDHCP = false;
+    # For management of the managed switch
+    interfaces.enu1u1.ipv4.addresses = [{
+      address = "192.168.0.1";
+      prefixLength = 24;
+    }];
+
+    # For internet
     interfaces.vlan11.useDHCP = true;
   };
 
